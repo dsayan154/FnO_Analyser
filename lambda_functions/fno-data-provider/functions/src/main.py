da73affa31074321392c60987332dd3fd2d5e86d
dsayan154 <sdas@opengov.com>
2022-07-17 12:23:32 +0530

    remove old files and lambda_handler added

--- lambda_functions/fno-data-provider/functions/src/main.py
+++ lambda_functions/fno-data-provider/functions/src/main.py
@@ -34,7 +34,7 @@ def updateOptionChain(instrDetails: dict, **kwargs):
 def raiseForWrongValue(msg:str = 'Wrong Value Provided'):
   raise ValueError(msg)
 
-if __name__ == '__main__':
+def start():
   purpose = os.getenv('PURPOSE', 'UPDATE_OPTION_CHAIN')
   instrmntKind = os.getenv('INSTRUMENT_KIND', 'STOCK')
   dbDetails = {  
@@ -84,3 +84,28 @@ if __name__ == '__main__':
   if purposeFunc is None:
     raiseForWrongValue()
   purposeFunc(instrmntDetails, **extraArgs)
+
+if __name__ == '__main__':
+  start()
+
+def lambda_handler(event, context):
+    """Sample Lambda function which mocks the operation of checking the current price 
+    of a stock.
+
+    For demonstration purposes this Lambda function simply returns 
+    a random integer between 0 and 100 as the stock price.
+
+    Parameters
+    ----------
+    event: dict, required
+        Input event to the Lambda function
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+    Returns
+    ------
+        dict: Object containing the current price of the stock
+    """
+    # Check current price of the stock
+    start()
